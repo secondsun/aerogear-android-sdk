f1bf00d8342947f3196e92c7ea139f4442526060
Summers Pittman <secondsun@gmail.com>
2018-05-10 10:36:41 -0400

    fixup

--- reactive/src/main/java/org/aerogear/mobile/reactive/Canceller.java
+++
@@ -1,9 +0,0 @@
-package org.aerogear.mobile.reactive;
-
-/**
- * This method provides a function which will actually cancel a request. The default function is to
- * interrupt the thread which is running.
- */
-public interface Canceller {
-    void doCancel();
-}
